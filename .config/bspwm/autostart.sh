dd06b13a72f601ebeb2ad9b18400c6130f8c92b8
Eduardo Zhizhpon <eduzhizhpon@gmail.com>
2023-09-19 20:24:44 -0500

    updated config

--- .config/bspwm/autostart.sh
+++ .config/bspwm/autostart.sh
@@ -3,38 +3,6 @@
 # BSPWM
 sh ~/.config/bspwm/bspwmrc &
 
-# Tap to click and Natural scrolling
-#sh ~/.config/scripts/xinput-touchpad-props.sh Touchpad
-
-# Composer
-#killall picom
-#picom --experimental-backends &
-#picom --config ~/.config/bspwm/config/picom.conf &
-
-# Notification - dunts
-#killall dunst
-#dunst &
-
-# Reload nvidia backlight
-# sh /opt/nvidia-settings/nvidia-brightness-load &
-
-#IBus
-#ibus-daemon -drxR
-
 # Autostart apps
 killall megasync
-#megasync & > /dev/null
-
-# Redshift
-# redshift -P -O 5000
-# redshift -x # neutral
-
-
-#
-# GTK 4 dark theme
-#
-#gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-
-
-
-#/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+megasync & > /dev/null
