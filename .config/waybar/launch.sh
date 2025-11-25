653f8f7531278c8b8b0500c8f20a5ba5d1966063
Eduardo Zhizhpon <eduzhizhpon@gmail.com>
2025-11-24 22:50:07 -0500

    feat: waybar

---
+++ .config/waybar/launch.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+waybar_path="$HOME/.config/waybar"
+
+exec 200>/tmp/waybar-launch.lock
+flock -n 200 || exit 0
+
+killall -q waybar
+while pgrep -x waybar >/dev/null; do sleep 0.1; done
+sleep 0.5
+
+style_path="$waybar_path/style.css"
+modules_path="$waybar_path/modules.json"
+primary_config_path="$waybar_path/configs/primary.json"
+secondary_config_path="$waybar_path/configs/secondary.json"
+generated_config_path="/tmp/waybar-config.jsonc"
+
+# Get monitor info from hyprctl
+mapfile -t monitors < <(hyprctl monitors -j | jq -r '.[] | .name')
+primary_monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
+
+modules_config=$(cat "$modules_path")
+primary_specific_config=$(cat "$primary_config_path")
+secondary_specific_config=$(cat "$secondary_config_path")
+
+configs="[]"
+
+base_config=$(jq -s '.[0] * .[1]' <(echo "$modules_config") <(echo "$primary_specific_config"))
+
+for monitor in "${monitors[@]}"; do
+  if [[ "$monitor" == "$primary_monitor" ]]; then
+    # Add output to the base primary config
+    config=$(jq --arg output "$monitor" '. + {output: $output}' <(echo "$base_config"))
+  else
+    # Merge with secondary config and add output
+    config=$(jq -s --arg output "$monitor" '.[0] * .[1] | . + {output: $output}' <(echo "$base_config") <(echo "$secondary_specific_config"))
+  fi
+  configs=$(echo "$configs" | jq ". + [$config]")
+done
+
+echo "$configs" > "$generated_config_path"
+
+waybar -c "$generated_config_path" -s "$style_path" &
+
+# Explicitly release the lock (optional) -> flock releases on exit
+flock -u 200
+exec 200>&-
