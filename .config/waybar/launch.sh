e77a319564dda04a9b500a20dbe32c305716476e
Eduardo Zhizhpon <eduzhizhpon@gmail.com>
2025-11-25 21:50:47 -0500

    feat: waybar monitors

--- .config/waybar/launch.sh
+++ .config/waybar/launch.sh
@@ -14,11 +14,10 @@ modules_path="$waybar_path/modules.json"
 primary_config_path="$waybar_path/configs/primary.json"
 secondary_config_path="$waybar_path/configs/secondary.json"
 generated_config_path="/tmp/waybar-config.jsonc"
+primary_monitor_path="$waybar_path/monitors/$DEVICE.json"
 
 # Get monitor info from hyprctl
 mapfile -t monitors < <(hyprctl monitors -j | jq -r '.[] | .name')
-primary_monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
-
 modules_config=$(cat "$modules_path")
 primary_specific_config=$(cat "$primary_config_path")
 secondary_specific_config=$(cat "$secondary_config_path")
@@ -26,6 +25,22 @@ secondary_specific_config=$(cat "$secondary_config_path")
 configs="[]"
 
 base_config=$(jq -s '.[0] * .[1]' <(echo "$modules_config") <(echo "$primary_specific_config"))
+
+if [ -f "$primary_monitor_path" ]; then
+  echo "Reading monitor config: $primary_monitor_path"
+  primary_monitor_config=$(cat "$primary_monitor_path")
+else
+  primary_monitor_config="{}"
+fi
+
+primary_monitor=$(echo "$primary_monitor_config" | jq -r '.name')
+
+if [ -z "$primary_monitor" ]; then
+  echo "Searching primary monitor"
+  primary_monitor=$(hyprctl monitors -j | jq -r '.[] | select(.focused == true) | .name')
+fi
+
+echo "Primary monitor: $primary_monitor"
 
 for monitor in "${monitors[@]}"; do
   if [[ "$monitor" == "$primary_monitor" ]]; then
