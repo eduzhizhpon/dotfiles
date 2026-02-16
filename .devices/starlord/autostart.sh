9679d0b7d99b18b745607ca440b5f0107fd7f278
Eduardo Zhizhpon <eduzhizhpon@gmail.com>
2026-02-16 14:16:50 -0500

    feat: megasync service

--- .devices/starlord/autostart.sh
+++ .devices/starlord/autostart.sh
@@ -20,12 +20,6 @@ fi;
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 
-# Autostart apps
-if [ "$(command -v megasync)" ]; then
-	killall megasync
-	megasync & > /dev/null
-fi
-
 # RClone to sync OneDrive
 # if [ "$(command -v rclone)" ]; then 
 	# rclone --vfs-cache-mode writes mount OneDrive:sql-scripts ~/Documents/sql-scripts & > /dev/null
