788ddeb56f852ed4f3a779db7f18ee775dd47dee
Eduardo Zhizhpon <eduzhizhpon@gmail.com>
2026-02-22 11:10:39 -0500

    feat: cliphist

--- .devices/scarlet/autostart.sh
+++ .devices/scarlet/autostart.sh
@@ -24,12 +24,6 @@ killall copyq
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 
-# Autostart apps
-if [ "$(command -v megasync)" ]; then
-	killall megasync
-	megasync & > /dev/null
-fi
-
 # RClone to sync OneDrive
 # if [ "$(command -v rclone)" ]; then 
 	# rclone --vfs-cache-mode writes mount OneDrive:sql-scripts ~/Documents/sql-scripts & > /dev/null
